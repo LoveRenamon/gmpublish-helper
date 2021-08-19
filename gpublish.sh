18ee367d5f94b0dfabcc9ec806d6418e703a7457
Davi (Debiddo) Gooz de Souza Filho <Love_Renamon@hotmail.com>
2021-08-19 12:11:13 -0300

    Update README.md
    Added gmad symlink example. 
    updated to match the usage to simplyfy it

--- gpublish.sh
+++ gpublish.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export VPROJECT="/mnt/500GB/source1"
+#!/bin/bash
 usage="Bad usage. Maybe you haven't a correct structure.
 
 - 1xcompiled should have your '$1' folder
 
-- 1xcompiled/$1 should have your 'id' txt file for push addon updates
+- 1xcompiled/$1/addon.json should have your 'workshopid' for push addon updates
 - 2xgma should have your '$1.gma' file for push updates
 
 command usage: gpublish <folder>"
@@ -12,22 +13,22 @@ command usage: gpublish <folder>"
 cd $VPROJECT
 
 # check if we write the folder name and if this folder really exist
-if [[ ! -z $1 && -d $VPROJECT/1xcompiled/$1 ]]; then
-  addon="$VPROJECT/2xgma/$1.gma"
-  icon="$VPROJECT/1xcompiled/$1/addon.jpg"
+if [[ ! -z $1 && -d $VPROJECT1/$1 ]]; then
+  addon="$GMA/$1.gma"
+  icon="$VPROJECT1/$1/addon.jpg"
 
 # always create a new gma
-  if [ -f $VPROJECT/1xcompiled/$1/addon.json ]; then
+  if [ -f $VPROJECT1/$1/addon.json ]; then
 
-    id="$VPROJECT/1xcompiled/$1/addon.json"
+    id="$VPROJECT1/$1/addon.json"
     workshopid=$( cat $id | jq '.workshopid' )
-    json=$( cat "$VPROJECT/1xcompiled/$1/addon.json" )
+    json=$( cat "$VPROJECT1/$1/addon.json" )
 
 # print json table
-echo $json
+#echo "$json"
 
 # now create our gma file
-gmad create -folder $VPROJECT/1xcompiled/$1 -out $VPROJECT/2xgma/$1.gma
+gmad create -folder $VPROJECT1/$1 -out $GMA/$1.gma
 
 else
   echo "addon.json file not found"
@@ -35,12 +36,11 @@ else
 fi
 
 # exit if no .gma was created
-if [ ! -f $addon ] then
+if [ ! -f $addon ]; then
   echo "$addon not found."
   exit 0
 
-elif [[ -f $icon && ! -z $workshopid || $workshopid = 0 ]]; then
-
+elif [[ -f $icon && -z $workshopid || $workshopid = 0 ]]; then
 echo "We'll publish a new addon
   Icon path: $icon
   Addon path: $addon"
@@ -48,9 +48,7 @@ echo "We'll publish a new addon
 # publish
 gmpublish create -icon $icon -addon $addon
 
-
-elif [[ ! -z $workshopid && ! $workshopid = 0 && -f $icon ]]; then
-
+elif [[ $workshopid && ! $workshopid = 0 && -f $icon ]]; then
 echo "We'll update a addon and their icon
   Workshop ID: $workshopid
   Icon path: $icon
@@ -59,18 +57,16 @@ echo "We'll update a addon and their icon
 # update
 gmpublish update -id $workshopid -addon $addon -icon $icon
 
-
-elif [[ ! -z $workshopid && ! $workshopid = 0 ]]; then
+elif [[ $workshopid && ! $workshopid = 0 ]]; then
   echo "We'll just update a addon:
   Workshop ID: $workshopid
-  at $addon"
+  Addon path: $addon"
 
 # update
 gmpublish update -id $workshopid -addon $addon
 
-
 else
-# Something is wrong
+# You fucked something
   echo "$usage"
 fi
 else
