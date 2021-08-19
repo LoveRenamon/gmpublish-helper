d47f3fc82ea1ca8a335f5329a5d8fe46d088f663
Davi (Debiddo) Gooz de Souza Filho <Love_Renamon@hotmail.com>
2021-08-19 02:21:33 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# gmpublish-helper
-A ugly bash script implementation for Garry's Mod gmpublish binary and publish and upgrade workshop addons through terminal
+# GMPublish-helper
+And why in hell I would need this? 
+Well, since I keep all my already compiled `vProjects` into a single folder called `"1xcompiled"` and crowbar isn't fast and stable with WINE. 
+I would like to push updates to workshop through a single command and inside my terminal. Making easier my addon maintances. 
+ 
+With my poorly skills I did this to help me. 
+ 
+ 
+## How to use: 
+  - You need be online at your Steam account. 
+  - Download this script and put where you wish to. *(I personally directly at `$HOME/.local/bin` to directly call from any terminal instance)* 
+  - Make sure your `gmpublish` and `gmad` are visible by adding a symlink to your `gmpublish` binary. example: 
+```bash 
+ln -s $HOME/.steam/steam/steamapps/common/GarrysMod/bin/linux64/gmpublish $HOME/.local/bin/gmpublish
+ln -s $HOME/.steam/steam/steamapps/common/GarrysMod/bin/linux64/gmad $HOME/.local/bin/gmad
+``` 
+  - Make sure GMod `bin/linux64` folder is visible to your `$LD_LIBRARY_PATH` variable. 
+```bash
+export LD_LIBRARY_PATH="$HOME/.steam/steam/steamapps/common/GarrysMod/bin/linux64:$LD_LIBRARY_PATH"
+``` 
+  - Edit exports and paths as you envirovment need, *(since mostly people have their own different methods)* 
+  - Call the script, with `gpublish.sh <foldername>` to call and parse the **addon.json** and **addon.jpg**. 
+    - If `workshopid` exist inside your **addon.json**, the script will automatically read the id inside and push a upgrade instead. 
+ 
+ 
+## Update a existing addon: 
+  just add `"workshopid": <id>` at your addon.json
---
+++ gpublish.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+export VPROJECT="/mnt/500GB/source1"
+export usage="Bad usage. Maybe you haven't a correct structure.
+
+- 1xcompiled should have your '$1' folder
+
+- 1xcompiled/$1 should have your 'id' txt file for push addon updates
+- 2xgma should have your '$1.gma' file for push updates
+
+command usage: gpublish <folder>"
+
+cd $VPROJECT
+
+# check if we write the folder name and if this folder really exist
+if [[ $1 && -d $VPROJECT/1xcompiled/$1 ]]; then
+  id="$VPROJECT/1xcompiled/$1/id.txt" # parse check if the addon already exist. you need to manually create for each already existent addon
+  workshopid=$( cat "$VPROJECT/1xcompiled/$1/id.txt" ) # where is stored the addon id for push updates
+  addon="$VPROJECT/2xgma/$1.gma" # our .gma file to write/push
+  icon="$VPROJECT/1xcompiled/$1/addon.jpg" # icon, Steam workshop require a 1:1 .jpg file
+
+# we always create a new gma
+  if [ -f $VPROJECT/1xcompiled/$1/addon.json ]; then
+
+json=$( cat "$VPROJECT/1xcompiled/$1/addon.json" )
+
+#echo "json:
+#$json
+#"
+
+gmad create -folder $VPROJECT/1xcompiled/$1 -out $VPROJECT/2xgma/$1.gma
+#echo "done"
+
+else
+  echo "addon.json file not found"
+  exit 0
+fi
+
+
+if [[ ! -f $id && -f $icon && -f $addon ]]; then
+
+echo "We'll create a new gma file and publish
+  Icon path: $icon
+  Addon path: $addon"
+
+# publish
+gmpublish create -icon $icon -addon $addon
+#echo "done"
+
+
+elif [[ -f $id && -f $icon && -f $addon ]]; then
+
+echo "We'll update a addon and icon
+  Workshop ID: $workshopid
+  Icon path: $icon
+  Addon path: $addon"
+
+# update
+gmpublish update -id $workshopid -addon $addon -icon $icon
+
+
+elif [[ -f $id && -f $addon ]]; then
+  echo "We'll just update a addon:
+  Workshop ID: $workshopid
+  at $addon"
+
+# update
+gmpublish update -id $workshopid -addon $addon
+
+
+else
+# You fucked something
+  echo "$usage"
+fi
+else
+# You really did something very very wrong
+  echo "$usage"
+fi
